ba9c03105055eaffa04dc2591a1d9cae3485bcdc
ajperlis07 <ajperlis07@gmail.com>
2021-01-19 09:42:40 -0500

    commit tree 5c021da1bdd54c2330610ffdccda4a56e908d820

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
 
+
+
+class CashRegister
+    attr_reader :total
+    attr_accessor :discount
+
+    def initialize(total, discount)
+        @total = total
+        @discount = discount
+    end
+end
+
